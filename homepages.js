7f0fba8ac53115839a7858178032db2090a47bbe
HaiIT <haimarjotit@gmail.com>
2026-02-02 17:49:23 +0700

    update_lan3_2/2/26

--- homepages.js
+++ homepages.js
@@ -5,4 +5,45 @@ menuBtn.addEventListener("click", () => {
   sideMenu.classList.toggle("active");
 });
 
+//Người dùng
+const avatarBtn = document.getElementById("avatarBtn");
+const userMenu = document.getElementById("userMenu");
+const profileBtn = document.getElementById("profileBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+
+/* mở / đóng menu */
+avatarBtn.onclick = () => {
+  userMenu.style.display =
+    userMenu.style.display === "block" ? "none" : "block";
+};
+
+/* đóng khi click ra ngoài */
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".user-wrapper")) {
+    userMenu.style.display = "none";
+  }
+});
+
+/* nút thông tin */
+profileBtn.onclick = () => {
+  alert("Mở trang thông tin cá nhân");
+  // window.location.href = "profile.html";
+};
+
+/* logout */
+logoutBtn.onclick = () => {
+  alert("Đăng xuất");
+};
+logoutBtn.onclick = () => {
+    alert("Đăng xuất");
+  // nếu có lưu đăng nhập
+  localStorage.removeItem("user");
+  sessionStorage.clear();
+
+  // quay về trang login
+  window.location.href = "login.html";
+};
+
+
+
 
--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function login() {
   const email = document.querySelector('input[type="text"]').value;
   const password = document.querySelector('input[type="password').value;
   if (email == "sinhvien@st.vlute.edu.vn" && password == "sv@k49") {
-    window.location.href = "home.html";
+    window.location.href = "homepages.html";
   } else {
     alert("Tài Khoản hoặc Mật Khẩu không đúng!");
   }
