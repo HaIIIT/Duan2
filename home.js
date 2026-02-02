43fa47f370f6dab05df338055d41b3c78a220ec2
HaiIT <haimarjotit@gmail.com>
2026-02-02 16:45:08 +0700

    update_lan1_2/2

---
+++ home.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById("menuBtn");
+const sideMenu = document.getElementById("sideMenu");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.toggle("active");
+});
+
+
---
+++ login.js
@@ -0,0 +1,9 @@
+function login() {
+  const email = document.querySelector('input[type="text"]').value;
+  const password = document.querySelector('input[type="password').value;
+  if (email == "sinhvien@st.vlute.edu.vn" && password == "sv@k49") {
+    window.location.href = "home.html";
+  } else {
+    alert("Tài Khoản hoặc Mật Khẩu không đúng!");
+  }
+}
